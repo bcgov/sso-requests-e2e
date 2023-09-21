99ae28adcf53630af76730301e7e16905831ff95
rstens <roland.stens@gmail.com>
2023-09-21 14:03:22 -0700

    House keeping and Update to readme.

--- testing/cypress/e2e/readcsv.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    cy.writeFile('cypress/fixtures/test.txt', '00008000\n', { flag: 'a+' })
-    cy.writeFile('cypress/fixtures/test.txt', '00008001\n', { flag: 'a+' })
-    cy.writeFile('cypress/fixtures/test.txt', '00008002\n', { flag: 'a+' })
-
-  })
-})
