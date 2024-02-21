88af102cc4d58af31ce2e73c382b6837ef7eb7f7
rstens <roland.stens@gmail.com>
2024-02-20 16:49:20 -0800

    fix: saml test

--- testing/cypress/e2e/disabled-idpstopper-060-saml.cy.ts
+++ testing/cypress/e2e/disabled-idpstopper-060-saml.cy.ts
@@ -1,58 +1,87 @@
 import path from 'path';
+import data from '../fixtures/idpstoppersaml.json'; // The data file will drive the tests
+import Request from '../appActions/Request';
 import Utilities from '../appActions/Utilities';
 let util = new Utilities();
 
-describe('SAML Spec', () => {
-  it('Gets Configuration', () => {
-    cy.setid(null).then(() => {
-      cy.login(null, null, null, null);
-    });
-    cy.contains('td', '00010968').parent().click();
-    cy.wait(1000);
-    cy.get('#rc-tabs-1-tab-tech-details').click();
-    cy.get('div').contains('Installation JSONs').should('be.visible');
-    cy.get('button').contains('Download').should('be.visible');
-    cy.get('button').contains('Download').focus().realClick();
-    cy.logout(null);
-  });
+var kebabCase = require('lodash.kebabcase');
 
-  it('Applies Config and Test', () => {
-    const downloadsFolder = Cypress.config('downloadsFolder');
-    const filePath = path.join(downloadsFolder, 'SAML Test 1-installation-dev.json');
+let testData = data;
+let tempData = data;
 
-    if (cy.setid('default')) {
-      cy.readFile(filePath)
-        .should('exist')
-        .then((content) => {
-          const jsonData = content;
-          // Extracting the properties into variables
-          const singleSignOnServiceURL = jsonData['Single Sign-On Service URL'];
-          const singleLogoutServiceURL = jsonData['Single Logout Service URL'];
-          const serviceProviderEntityID = jsonData['Service Provider Entity ID'];
-          const x509Certificate = jsonData['X.509 Certificate'];
-
-          cy.visit('http://localhost:8080');
-          cy.get('#signOnUrl', { timeout: 10000 }).clear().type(singleSignOnServiceURL);
-          cy.get('#logoutUrl', { timeout: 10000 }).clear().type(singleLogoutServiceURL);
-          cy.get('#entityId', { timeout: 10000 }).clear().type(serviceProviderEntityID, { log: false });
-          cy.get('#x509Cert', { timeout: 10000 }).clear().type(x509Certificate, { log: false });
-          cy.get('button[type="submit"]').click();
-
-          const sentArgs = { user: Cypress.env('username'), pass: Cypress.env('password') };
-          cy.origin(Cypress.env('siteminder'), { args: sentArgs }, ({ user, pass }) => {
-            cy.get('#login-to').contains('Log in to ').should('be.visible');
-            cy.get('#user').type(user);
-            cy.get('#password').type(pass, { log: false });
-            cy.get('input[name=btnSubmit]').click();
-            cy.wait(1000);
-          });
+describe('Run IDP Stopper SAML Test', () => {
+  testData.forEach((data, index) => {
+    // Only run the test if the smoketest flag is set and the test is a smoketest
+    if (util.runOk(data)) {
+      let req = new Request();
+      it(`Create ${data.create.projectname} (Test ID: ${data.create.test_id}) - ${data.create.description}`, () => {
+        cy.setid('default').then(() => {
+          cy.login(null, null, null, null);
         });
-      cy.get('#myTabContent').within(() => {
-        cy.get('#assertions-tab-pane').within(() => {
-          cy.contains('td', 'family_name').siblings().contains('SSO Training').should('be.visible');
-        });
+        req.showCreateContent(data);
+        req.populateCreateContent(data);
+        req.createRequest();
       });
-      cy.get('a[role="button"]', { timeout: 10000 }).contains('Logout').click();
+
+      // Using the SAML Playground to test the IDP Stopper
+      it('Get configuration', () => {
+        cy.contains('td', Cypress.env('test')).parent().click();
+        cy.wait(1000);
+        // Download the JSON file
+        cy.get('#rc-tabs-1-tab-tech-details').click();
+        cy.get('div').contains('Installation JSONs').should('be.visible');
+        cy.get('button').contains('Download').should('be.visible');
+        cy.get('button').contains('Download').focus().realClick();
+        cy.logout(null);
+      });
+      it('Applies Config and Test', () => {
+        const downloadsFolder = Cypress.config('downloadsFolder');
+        const filePath = path.join(downloadsFolder, req.projectName + '-installation-dev.json');
+
+        if (cy.setid('default')) {
+          cy.readFile(filePath)
+            .should('exist')
+            .then((content) => {
+              const jsonData = content;
+              // Extracting the properties into variables
+              const singleSignOnServiceURL = jsonData['Single Sign-On Service URL'];
+              const singleLogoutServiceURL = jsonData['Single Logout Service URL'];
+              const serviceProviderEntityID = jsonData['Service Provider Entity ID'];
+              const x509Certificate = jsonData['X.509 Certificate'];
+
+              cy.visit('http://localhost:8080');
+              cy.get('#signOnUrl', { timeout: 10000 }).clear().type(singleSignOnServiceURL);
+              cy.get('#logoutUrl', { timeout: 10000 }).clear().type(singleLogoutServiceURL);
+              cy.get('#entityId', { timeout: 10000 }).clear().type(serviceProviderEntityID, { log: false });
+              cy.get('#x509Cert', { timeout: 10000 }).clear().type(x509Certificate, { log: false });
+              cy.get('button[type="submit"]').click();
+
+              const sentArgs = { user: Cypress.env('username'), pass: Cypress.env('password') };
+              cy.origin(Cypress.env('siteminder'), { args: sentArgs }, ({ user, pass }) => {
+                cy.get('#login-to').contains('Log in to ').should('be.visible');
+                cy.get('#user').type(user);
+                cy.get('#password').type(pass, { log: false });
+                cy.get('input[name=btnSubmit]').click();
+                cy.wait(1000);
+              });
+            });
+          cy.get('#myTabContent').within(() => {
+            cy.get('#assertions-tab-pane').within(() => {
+              cy.contains('td', 'family_name').siblings().contains('SSO Training').should('be.visible');
+            });
+          });
+          cy.get('a[role="button"]', { timeout: 10000 }).contains('Logout').click();
+        }
+      });
+
+      it('Delete the request', () => {
+        cy.setid('default').then(() => {
+          cy.login(null, null, null, null);
+        });
+        let req = new Request();
+        req.deleteRequest(Cypress.env('test'));
+        cy.logout(null);
+      });
     }
   });
 });
