b43ac11446001b27686cec136c53a8408c6b7c61
rstens <roland.stens@gmail.com>
2024-02-28 10:28:25 -0800

    fix: change delete all integration

--- testing/cypress/e2e/integration-990-deleteAllIntegrations.cy.ts
+++ testing/cypress/e2e/integration-990-deleteAllIntegrations.cy.ts
@@ -1,22 +1,12 @@
 import Request from '../appActions/Request';
 
 describe('Delete All Integrations', () => {
-  let req = new Request();
-  it('Delete All Requests as admin', function () {
-    cy.setid('admin').then(() => {
-      cy.login(null, null, null, null);
-    });
-    req.deleteAllRequests();
-    cy.logout(null);
-    cy.clearAllSessionStorage();
-  });
-
   it('Delete All Requests as default user', function () {
     cy.setid(null).then(() => {
       cy.login(null, null, null, null);
     });
+    let req = new Request();
     req.deleteAllRequests();
     cy.logout(null);
-    cy.clearAllSessionStorage();
   });
 });
