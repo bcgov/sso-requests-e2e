d2b5aebbace651ab3b17d44f29c1369f227b382a
rstens <roland.stens@gmail.com>
2024-03-21 18:01:44 -0700

    fix: smooth out teams

--- testing/waitforconnection.sh
+++ testing/waitforconnection.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# max_attempts=3
-# wait_time=15
-# url=https://api-biohubbc.apps.silver.devops.gov.bc.ca/version
-# code=200
+max_attempts=3
+wait_time=15
+url=https://bcgov.github.io/sso-requests-sandbox
+code=301
 if [ $(curl --output /dev/null --silent --head -X GET --retry ${max_attempts} --fail --retry-all-errors --retry-delay ${wait_time} --retry-max-time 240 -w "%{response_code}\n" ${url}) -eq ${code} ]; then
 	echo "Connection Success!"
 else
