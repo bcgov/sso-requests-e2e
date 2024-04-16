6b62c2d1b358393c5a000c9dc00449bb3b51e052
jonathan langlois <jonathan.langlois@gov.bc.ca>
2024-04-16 12:53:49 -0700

    fix: comment
    move comment to correct location

--- testing/cypress/appActions/Request.ts
+++ testing/cypress/appActions/Request.ts
@@ -226,10 +226,10 @@ class Request {
       }
     });
 
-    // Get the ID of the request just created make it available to the class
-    // and write it to a file, so that it can be deleted later.
     cy.get(this.reqPage.integrationsTableStatus).contains('Completed');
 
+    // Get the ID of the request just created make it available to the class
+    // and write it to a file, so that it can be deleted later.
     cy.get(this.reqPage.integrationsTable)
       .first()
       .then(($id) => {
