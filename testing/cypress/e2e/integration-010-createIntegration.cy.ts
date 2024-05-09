4cab2d539609abcb7676e61562b5870b7b5ecd5f
jonathan langlois <jonathan.langlois@gov.bc.ca>
2024-05-09 13:55:37 -0700

    chore: update
    updates

--- testing/cypress/e2e/integration-010-createIntegration.cy.ts
+++ testing/cypress/e2e/integration-010-createIntegration.cy.ts
@@ -14,29 +14,55 @@ describe('Create Integration Requests', () => {
     });
   });
 
+  const cleanup = () => {
+    const req = new Request();
+    cy.clearAllCookies();
+    cy.setid(null).then(() => {
+      cy.login(null, null, null, null);
+    });
+    req.deleteAllRequests();
+    cy.logout(null);
+  };
+
   afterEach(() => {
     cy.logout(null);
   });
-  before(() => {
-    cy.cleanGC();
-  });
 
+  before(() => {
+    cleanup();
+  });
   after(() => {
-    cy.writeFile('cypress/fixtures/requestsafter.json', tempData);
-    cy.cleanGC();
+    cleanup();
   });
 
   // Iterate through the JSON file and create a team for each entry
   // The set up below allows for reporting on each test case
   testData.forEach((data, index) => {
     if (util.runOk(data)) {
+      let req = new Request();
+
       it(`Create ${data.create.projectname} (Test ID: ${data.create.test_id}) - ${data.create.description}`, () => {
-        let req = new Request();
         req.showCreateContent(data);
         req.populateCreateContent(data);
         cy.wrap(req.createRequest()).then(() => {
           tempData[index].id = Cypress.env(util.md5(data.create.projectname));
         });
+      });
+
+      it(`Validate creation of ${data.create.projectname}`, () => {
+        req.validateRequest(data.id);
+      });
+
+      it(`Update ${data.create.projectname}`, () => {
+        req.populateUpdateContent(data.id);
+        req.updateRequest(data.id);
+      });
+
+      it(`Validate update of ${data.create.projectname}`, () => {
+        req.populateUpdateValidationContent(data);
+        data.id;
+        req.updateRequest(data.id);
+        req.validateRequest(req.id);
       });
     }
   });
